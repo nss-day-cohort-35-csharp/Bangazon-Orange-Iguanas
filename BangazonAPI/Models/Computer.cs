f5870918f1bff9fd1bedf931912862d01cfcc19c
David <dcornish84@gmail.com>
2019-12-19 13:12:06 -0600

    created computer and department models

---
+++ BangazonAPI/Models/Computer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Computer
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; }
+
+        public DateTime PurchaseDate { get; set; }
+
+        public DateTime DecomissionDate { get; set; }
+    }
+}
---
+++ BangazonAPI/Models/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+
+        public int Budget { get; set; }
+
+        public string DepartmentName { get; set; }
+
+    }
+}
