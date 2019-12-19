e67cafb2cb4fe72e287ee50369a965904e889839
stephensenft21 <stephensenft21@gmail.com>
2019-12-19 14:02:48 -0600

    made changes to customer and userpayment type

--- BangazonAPI/Models/Customer.cs
+++ BangazonAPI/Models/Customer.cs
@@ -7,5 +7,20 @@ namespace BangazonAPI.Models
 {
     public class Customer
     {
+
+
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Email { get; set; }
+        public int Phone { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public bool Active { get; set; }
+
+
     }
 }
--- BangazonAPI/Models/UserPaymentType.cs
+++ BangazonAPI/Models/UserPaymentType.cs
@@ -7,5 +7,12 @@ namespace BangazonAPI.Models
 {
     public class UserPaymentType
     {
+
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int AcctNumber { get; set; }
+        public bool Active { get; set; }
+
     }
 }
